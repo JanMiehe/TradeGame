00d3ab22f41ee1b9f5804ee9336f9bc83f83819c
JanMiehe <miehejan8@gmail.com>
2025-02-09 14:17:41 -0600

    1

--- main.cpp
+++ main.cpp
@@ -1,55 +1,44 @@
 #include <iostream>
 #include <cstdlib>
+#include <ctime>
 using namespace std;
 
 bool game = true;
-int money=500;
+int money = 500;
 int input;
 int win;
 int lose;
-int persentache;
+double percentage;
 
-int main(){
-    srand(time(0)); // the random number is generated from hier
-    
-    win =  rand()%100;
-    
-    lose = 100 - win;
+int main() {
+    srand(time(0)); // Seed once at the beginning
 
+    cout << "-- This is a test to identify if you are a solo or a single Player --" << endl;
 
-   cout << "-- This is a test to identify if you are a solo or a single Player --" << endl;
-
-    while(game){
-
-         srand(time(0)); // the random number is generated from hier
-    
-         win =  rand()%100;
-    
+    while (game) {
+        win = rand() % 100;
         lose = 100 - win;
+        percentage = rand() % 100;
 
+        cout << win << "% win: +500$ " << lose << "% lose: -200$" << endl;
+        cout << "Type (1) to Play \nType (2) to Exit" << endl;
+        cin >> input;
 
-
-
-        cout<<win<<"% win: 500$ "<<lose<<"% lose: -200$"<<endl;
-
-        cout << "Type (1) for Option 1 \nType (2) for option2" << endl;
-
-        cin>>input;
-
-
-
-        persentache=rand()%100;
-
-
-        if (input==1){
-            if(persentache<=win){
-                money+=500;
-                cout<<money;
+        if (input == 1) {
+            if (percentage < win) {
+                money += 500;
+                cout << "You won! Current money: " << money << endl;
+            } else {
+                money -= 200;
+                cout << "You lost! Current money: " << money << endl;
             }
-            else{
-                money-200;
-                cout<<money;
-            }
-    }   
+        } else if (input == 2) {
+            game = false; // Exit the game
+            cout << "Exiting game. Final money: " << money << endl;
+        } else {
+            cout << "Invalid input. Try again." << endl;
+        }
+    }
+
     return 0;
 }
