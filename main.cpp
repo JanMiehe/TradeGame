8e7da1d89d1d636ab46eb6a2482b58bfef7ab7ab
Pelutson <marc.miehe@icloud.com>
2025-02-09 16:23:12 +0100

    Added some details

--- main.cpp
+++ main.cpp
@@ -5,21 +5,58 @@ using namespace std;
 int money=500;
 int input;
 int win;
+bool game = true;
 int lose;
 
 int main(){
-    srant(time(0));
-    win=rand()%100;
-    lose=rand()%100;
-    cout<<win<<"% win:500$ "<<lose<<"% lose:-200$"<<endl;
-    cin>>input;
-    if (input==1){
-        money+=500;
-        cout<<money<<rand()%100;
-    }
-    else{
-        cout<<"Your coc sucker you can only write 1,2 or 3!!!";
-    }
+    srand(time(0)); // the random number is generated from hier
+    
+    win =  rand()%100;
+    
+    lose = 100 - win;
+
+
+   cout << "-- This is a test to identify if you are a solo or a single Player --" << endl;
+
+    while(game){
+
+         srand(time(0)); // the random number is generated from hier
+    
+         win =  rand()%100;
+    
+        lose = 100 - win;
+
+
+
+
+        cout<<win<<"% win: 500$ "<<lose<<"% lose: -200$"<<endl;
+
+        cout << "Type (1) for Option 1 \nType (2) for option2" << endl;
+
+        cin>>input;
+
+
+
+
+
+        while(input !== 1 && !==2){
+
+            if (input==1){
+                
+                money+=500;
+                cout<<money<<rand()%100;
+                break;
+
+            }
+            else{
+
+
+                cout<<"Your coc sucker you can only write 1,2 or 3!!!";
+                continue;
+
+            }
+        }
 
     return 0;
+    }   
 }
