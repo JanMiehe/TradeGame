7ce0aa55ed84f565fbab5f085fd131ea91080139
JanMiehe <miehejan8@gmail.com>
2025-02-10 16:46:10 -0600

    Color
    added Color

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <cstdlib>
+#include <windows.h>
+#include <ctime>
+
+
 using namespace std;
 
 bool game = true;
@@ -11,16 +15,22 @@ int winMoney;
 int loseMoney;
 double persentache;
 
+int col(int a){
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+    return SetConsoleTextAttribute(h, a);
+    
+}
+
 int main(){
+
+    srand(time(0)); // the random number is generated from hier
     
    cout << "-- This is a test to identify if you are a solo or a single Player --" << endl;
    cout<<"bankacount: "<<money<<"$"<<endl<<endl;
 
     while(money > 0){
 
-         srand(time(0)); // the random number is generated from hier
-    
-         win =  rand()%100;
+        win =  rand()%100;
     
         lose = 100 - win;
         winMoney = rand()%10 * 100;
@@ -28,8 +38,11 @@ int main(){
 
         persentache = rand()%100;
 
-
-        cout<<win<<"% win: "<<winMoney<<"$ "<<lose<<"% lose: -"<<loseMoney<<"$ "<<endl;
+        col(2);
+        cout<<win<<"% win: "<<winMoney<<"$ ";
+        col(4);
+        cout<<lose<<"% lose: -"<<loseMoney<<"$ "<<endl;
+        col(7);
 
         cout << "[1] go trading [2] stay in harbor" << endl;
 
@@ -40,11 +53,18 @@ int main(){
         if (input==1){
             if(persentache<=win){
                 money+=winMoney;
-                cout<<"Ship returnd"<<endl<<"bankacount: "<<money<<"$"<<" +"<<winMoney<<"$"<<endl<<endl;
+                cout<<"Ship returnd"<<endl<<"bankacount: "<<money<<"$ ";
+                col(2);
+                cout<<"+"<<winMoney<<"$"<<endl<<endl;
+                col(7);
             }
             else{
                 money-=loseMoney;
-                cout<<"Ship losed"<<endl<<"bankacount: "<<money<<"$"<<" -"<<loseMoney<<"$"<<endl<<endl;
+                
+                cout<<"Ship losed"<<endl<<"bankacount: "<<money<<"$ ";
+                col(4);
+                cout<<"-"<<loseMoney<<"$"<<endl<<endl;
+                col(7);
             }
         }   
         
@@ -52,3 +72,4 @@ int main(){
     
     cout<<"bankruptcy";
     return 0;
+}
